3c44cb372fefc188701f148066dc9abdd230e160
WimEngelenn <wim.engelen@student.ap.be>
2019-06-03 20:28:37 +0200

    index nieuwe gebruiker

---
+++ javascript/startscherm_local_storage.js
@@ -0,0 +1,31 @@
+
+var remove_content_start = document.getElementById("slide-outer");
+
+var startscherm_content;
+
+var body = document.getElementById('introductie');
+
+function main() {
+  checkFirstVisitor();
+  body.style.display = "block";
+}
+
+function check_returning_visitor() {
+  localStorage.setItem("startscherm_content_ls", true);
+  checkFirstVisitor();
+
+}
+
+function checkFirstVisitor() {
+  startscherm_content = JSON.parse(localStorage.getItem("startscherm_content_ls"));
+
+ if(startscherm_content){
+    remove_content_start.style.display = "none";
+
+    window.setTimeout(function () {
+        location.href = "/basis/basis_start.html";
+    }, 2000);
+  }
+}
+
+main();
